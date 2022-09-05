3f625383ab099ba06012ac15eb34d64ce90d4a77
mohsylla <sylla.m7@gmail.com>
2022-09-05 16:12:51 +0200

    modif

---
+++ P10 final/Reciplease/Reciplease/View/ButtonRounded.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonRounded.swift
+//  Reciplease
+//
+//  Created by Sylla on 03/09/2022.
+//
+
+import UIKit
+
+class ButtonRounded: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ P9_sylla_mohamadou/baluchon/baluchon/View/ButtonCorner.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonCorner.swift
+//  Baluchon
+//
+//  Created by Sylla on 05/09/2022.
+//
+
+import UIKit
+
+class ButtonCorner: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
